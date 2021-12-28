c24314163625a924cd1d56d63b3154753f9ae0b9
Askhabov-HM <askhamzat@mail.ru>
2021-12-28 11:54:41 +0300

    beginner lesson 3

---
+++ pythonBeginner/3.py
@@ -0,0 +1,5 @@
+name = input('input user name ')
+age = int(input('input user age '))
+
+print('User name is %s, User age is %d'%(name, age), sep="***", end=' --end--\n')
+print('-1-','-2-','-3-', sep=' --end--\n', end=' --end--\n')
