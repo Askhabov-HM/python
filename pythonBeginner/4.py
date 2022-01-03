ff4eeb509f43fddb0179cf394152ee6274938eba
Askhabov-HM <askhamzat@mail.ru>
2022-01-03 20:36:05 +0300

    beginner lesson 4

---
+++ pythonBeginner/4.py
@@ -0,0 +1,8 @@
+import math
+#import math library
+
+print("min: %d"%min(1, 2, 3, -10))
+print("max: %d"%max(1, 2, 3, 556))
+print('вывод целого числа %d'%(3//2))
+print('возведение в степень %d'%(2**3))
+
