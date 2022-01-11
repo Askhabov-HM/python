bd4fc200b804e3f7cdafdea3b693192b2242fb43
Askhabov-HM <askhamzat@mail.ru>
2022-01-11 22:27:28 +0300

    beginner (Learn) lesson 25
    work with string formating in Python

---
+++ pythonBeginner/25.py
@@ -0,0 +1,9 @@
+from typing import NamedTuple
+
+
+print("my name is {0}. I'm {1} years old".format('N', 18))
+print("it is named variable in format: {namedVar}".format(namedVar='someVar'))
+
+name = 'A'
+age = 19
+print(f'my name is {name}. I\'m {age} years old')
